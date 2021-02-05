2bfe559a4957248605fbc1efd098817fb1681c60
antrob25 <55016689+antrob25@users.noreply.github.com>
2021-02-05 02:37:45 -0600

    Updated header for new board

--- SRAActuation/SRAActuation.h
+++ SRAActuation/SRAActuation.h
@@ -5,20 +5,20 @@
 #include "RoveStmVnhPwm.h"
 #include "RoveWatchdog.h"
 
-#define CHEM1_INA   PL_0
-#define CHEM1_INB   PL_1
+#define CHEM1_INA   PC_6
+#define CHEM1_INB   PE_5
 #define CHEM1_PWM   PF_1
 
-#define CHEM2_INA   PL_2
-#define CHEM2_INB   PL_3
+#define CHEM2_INA   PD_3
+#define CHEM2_INB   PC_7
 #define CHEM2_PWM   PF_2
 
-#define CHEM3_INA   PQ_2
-#define CHEM3_INB   PQ_3
-#define CHEM3_PWM   PK_4
+#define CHEM3_INA   PB_2
+#define CHEM3_INB   PB_3
+#define CHEM3_PWM   PF_3
 
-#define GENEVA_INA   PP_3
-#define GENEVA_INB   PQ_1
+#define GENEVA_INA   PD_4
+#define GENEVA_INB   PD_5
 #define GENEVA_PWM   PG_1
 
 #define CHEM1_SW    PG_0
