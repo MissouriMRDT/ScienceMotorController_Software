1c0f97c480435cf0165358e83885a6459217462e
antrob25 <55016689+antrob25@users.noreply.github.com>
2021-02-16 01:38:51 -0600

    Added unnecessary abstraction just to appease Thomas

--- SRAActuation/SRAActuation.h
+++ SRAActuation/SRAActuation.h
@@ -5,44 +5,99 @@
 #include "RoveStmVnhPwm.h"
 #include "RoveWatchdog.h"
 
+#define CHEM1_PIN1  CHEM_PIN1_1
+#define CHEM_PIN1_1 CHEM_1_INA
+#define CHEM1_PIN2  CHEM_PIN2_1
+#define CHEM_PIN2_1 CHEM_1_INB
+#define CHEM1_PIN3  CHEM_PIN3_1
+#define CHEM_PIN3_1 CHEM_1_PWM
+#define CHEM_1_INA  CHEM1_INA
+#define CHEM_1_INB  CHEM1_INB
+#define CHEM_1_PWM  CHEM1_PWM
 #define CHEM1_INA   PL_0
 #define CHEM1_INB   PL_1
 #define CHEM1_PWM   PF_1
 
+#define CHEM2_PIN1  CHEM_PIN1_2
+#define CHEM_PIN1_2 CHEM_2_INA
+#define CHEM2_PIN2  CHEM_PIN2_2
+#define CHEM_PIN2_2 CHEM_2_INB
+#define CHEM2_PIN3  CHEM_PIN3_2
+#define CHEM_PIN3_2 CHEM_2_PWM
+#define CHEM_2_INA  CHEM2_INA
+#define CHEM_2_INB  CHEM2_INB
+#define CHEM_2_PWM  CHEM2_PWM
 #define CHEM2_INA   PL_2
 #define CHEM2_INB   PL_3
 #define CHEM2_PWM   PF_2
 
+#define CHEM3_PIN1  CHEM_PIN1_3
+#define CHEM_PIN1_3 CHEM_3_INA
+#define CHEM3_PIN2  CHEM_PIN2_3
+#define CHEM_PIN2_3 CHEM_3_INB
+#define CHEM3_PIN3  CHEM_PIN3_3
+#define CHEM_PIN3_3 CHEM_3_PWM
+#define CHEM_3_INA  CHEM3_INA
+#define CHEM_3_INB  CHEM3_INB
+#define CHEM_3_PWM  CHEM3_PWM
 #define CHEM3_INA   PQ_2
 #define CHEM3_INB   PQ_3
 #define CHEM3_PWM   PK_4
 
+#define GENEVA_PIN1  GENEVA_PIN1_
+#define GENEVA_PIN1_ GENEVA__INA
+#define GENEVA_PIN2  GENEVA_PIN2_
+#define GENEVA_PIN2_ GENEVA__INB
+#define GENEVA_PIN3  GENEVA_PIN3_
+#define GENEVA_PIN3_ GENEVA__PWM
+#define GENEVA__INA   GENEVA_INA
+#define GENEVA__INB   GENEVA_INB
+#define GENEVA__PWM   GENEVA_PWM
 #define GENEVA_INA   PP_3
 #define GENEVA_INB   PQ_1
 #define GENEVA_PWM   PG_1
 
+#define SWITCH_1    CHEM1_SW
+#define SWITCH_2    CHEM2_SW
+#define SWITCH_3    CHEM3_SW
+#define SWITCH_4    GENEVA_SW
 #define CHEM1_SW    PG_0
 #define CHEM2_SW    PL_4
 #define CHEM3_SW    PE_1
 #define GENEVA_SW   PE_2
 
 RoveCommEthernet RoveComm;
-EthernetServer TCPServer(RC_ROVECOMM_ETHERNET_SRA_ACTUATIONBOARD_PORT);
+EthernetServer TCPServer(RC_ROVECOMM_SCIENCEACTUATIONBOARD_PORT);
 
 RoveStmVnhPwm Chem1Motor;
 RoveStmVnhPwm Chem2Motor;
 RoveStmVnhPwm Chem3Motor;
-
 RoveStmVnhPwm GenevaMotor;
 
 int16_t chem_Speeds[3] = { 0, 0, 0};
 int16_t geneva_Speed[1] = {0};
-uint8_t motorButtons[4] = {CHEM1_SW, CHEM2_SW, CHEM3_SW, GENEVA_SW};
+uint8_t motorButtons[4] = {SWITCH_1, SWITCH_2, SWITCH_3, SWITCH_4};
 
 RoveWatchdog  Watchdog;
-
 rovecomm_packet packet;
 
+void BeginSerial();
+void BeginRoveComm();
+void AttachChemMotors();
+void AttachGenevaMotors();
+void InitButtons();
+void AttachWatchdog();
+void BeginWatchdog();
+void ParsePackets();
+void ScienceActuationBoard_GenevaOpenLoop();
+void ScienceActuationBoard_Chemicals();
+int16_t* ReceiveGenevaSpeed();
+void PrintGenevaSpeed(int16_t* genevaSpeed);
+void DriveGenevaMotor(int16_t* genevaSpeed);
+int16_t* ReceiveChemSpeeds();
+void CheckButtons();
+void CheckButtonsEstop();
 void Estop();
+void WatchdogClear();
 
 #endif
--- SRAActuation/SRAActuation.ino
+++ SRAActuation/SRAActuation.ino
@@ -1,106 +1,181 @@
 #include "RoveComm.h"
 #include "RoveStmVnhPwm.h"
 #include "SRAActuation.h"
-
 #include "RoveWatchdog.h"
 
 void setup()
 {
-  Serial.begin(115200);
-
-
-  RoveComm.begin(RC_SRAACTUATIONBOARD_FOURTHOCTET, &TCPServer);
-
-  Chem1Motor.attach(CHEM1_INA, CHEM1_INB, CHEM1_PWM);  
-  Chem2Motor.attach(CHEM2_INA, CHEM2_INB, CHEM2_PWM);
-  Chem3Motor.attach(CHEM3_INA, CHEM3_INB, CHEM3_PWM);
-  GenevaMotor.attach(GENEVA_INA, GENEVA_INB, GENEVA_PWM); //motor 4
-
-  for(int i = 0; i < 4; i++)
-  {
-    pinMode(motorButtons[i], INPUT);
-  }
-  
-  //attach estop to watchdog, cut motors after 150 ms of no comms (control rate is 100ms)
-  Watchdog.attach(Estop);
-  Watchdog.start(150);
+  BeginSerial();
+  BeginRoveComm();
+  AttachChemMotors();
+  AttachGenevaMotors();
+  InitButtons();
+  AttachWatchdog();
+  BeginWatchdog();
 }
 
 void loop()
 {
+  ParsePackets();
+  CheckButtons();
+}
+
+void BeginSerial()
+{
+  Serial.begin(115200);
+}
+
+void BeginRoveComm()
+{
+  RoveComm.begin(RC_SCIENCEACTUATIONBOARD_FOURTHOCTET, &TCPServer);
+}
+
+void AttachChemMotors()
+{
+  Chem1Motor.attach(CHEM1_PIN1, CHEM1_PIN2, CHEM1_PIN3);  
+  Chem2Motor.attach(CHEM2_PIN1, CHEM2_PIN2, CHEM2_PIN3);
+  Chem3Motor.attach(CHEM3_PIN1, CHEM3_PIN2, CHEM3_PIN3);
+}
+
+void AttachGenevaMotors()
+{
+  GenevaMotor.attach(GENEVA_PIN1, GENEVA_PIN2, GENEVA_PIN3);
+}
+
+void InitButtons()
+{
+  for(int i = 0; i < 4; i++)
+  {
+    pinMode(motorButtons[i], INPUT);
+  }
+}
+
+void AttachWatchdog()
+{
+  Watchdog.attach(Estop);
+}
+
+void BeginWatchdog()
+{
+  Watchdog.start(150);
+}
+
+void ParsePackets()
+{
   packet = RoveComm.read();
-  //Serial.println(packet.data_id);
   switch (packet.data_id)
   {
-    case RC_SRAACTUATION_GENEVA_OPENLOOP_DATAID:
-      int16_t* genevaSpeed;
-      genevaSpeed = (int16_t*)packet.data;
-      Serial.println("Geneva Position: ");
-      Serial.println(genevaSpeed[0]);
-      geneva_Speed[0] = genevaSpeed[0];
-      Watchdog.clear();
+    case RC_SCIENCEACTUATIONBOARD_GENEVAOPENLOOP_DATA_ID:
+      ScienceActuationBoard_GenevaOpenLoop();
       break;
-    case RC_SRAACTUATION_CHEMICALS_DATAID:
-      int16_t* chemicalSpeeds;
-      chemicalSpeeds = (int16_t*)packet.data;
-      Serial.println("Chemical Speeds Position: ");
-      Serial.println(chemicalSpeeds[0]);
-      Serial.println(chemicalSpeeds[1]);
-      Serial.println(chemicalSpeeds[2]);
-      for(uint8_t i = 0; i < 3; i++)
-      {
-        chem_Speeds[i] = chemicalSpeeds[i];
-      }
-      Watchdog.clear();
+    case RC_SCIENCEACTUATIONBOARD_CHEMICALS_DATA_ID:
+      ScienceActuationBoard_Chemicals();
       break;
   }
+}
 
+void ScienceActuationBoard_GenevaOpenLoop()
+{
+  int16_t* genevaSpeed = ReceiveGenevaSpeed();
+  PrintGenevaSpeed(genevaSpeed);
+  DriveGenevaMotor(genevaSpeed);
+  WatchdogClear();
+}
+
+void ScienceActuationBoard_Chemicals()
+{
+  int16_t* chemicalSpeeds = ReceiveChemSpeeds();
+  PrintChemSpeed(chemicalSpeeds);
+  DriveChemMotor(chemicalSpeeds);
+  WatchdogClear();
+}
+
+int16_t* ReceiveGenevaSpeed()
+{
+  return (int16_t*)packet.data;
+}
+
+void PrintGenevaSpeed(int16_t* genevaSpeed)
+{
+  Serial.println("Geneva Position: ");
+  Serial.println(genevaSpeed[0]);
+}
+
+void DriveGenevaMotor(int16_t* genevaSpeed)
+{
+  GenevaMotor.drive(geneva_Speed[0]);
+}
+
+int16_t* ReceiveChemSpeeds()
+{
+  return (int16_t*)packet.data;
+}
+
+void PrintChemSpeed(int16_t* chemicalSpeeds)
+{
+  Serial.println("Chemical Speeds Position: ");
+  Serial.println(chemicalSpeeds[0]);
+  Serial.println(chemicalSpeeds[1]);
+  Serial.println(chemicalSpeeds[2]);
+}
+
+void DriveChemMotor(int16_t* chemicalSpeeds)
+{
+  Chem1Motor.drive(chemicalSpeeds[0]);
+  Chem2Motor.drive(chemicalSpeeds[1]);
+  Chem3Motor.drive(chemicalSpeeds[2]);
+}
+
+void CheckButtons()
+{
   for(int i = 0; i < 4; i++)
   {
     if(digitalRead(motorButtons[i]))
     {
       if(i == 3)
       {
-        geneva_Speed[0] = 140;
+        GenevaMotor.drive(140);
       }
       else
       {
-        chem_Speeds[i] = 140;
+        Chem1Motor.drive(140);
+        Chem2Motor.drive(140);
+        Chem3Motor.drive(140);
       }
-      Watchdog.clear();
+      WatchdogClear();
     }
   }
-  
-  //for(int i = 0; i < 3; i++)
-  //{
-  //  Serial.println(chem_Speeds[i]);
-  //}
-  //Serial.println(geneva_Speed[0]);
-  Chem1Motor.drive(chem_Speeds[0]);
-  Chem2Motor.drive(chem_Speeds[1]);
-  Chem3Motor.drive(chem_Speeds[2]); 
-  GenevaMotor.drive(geneva_Speed[0]);
 }
 
-void Estop()
+void CheckButtonsEstop()
 {
-  Serial.println("Estop triggered");
-  
   for(int i = 0; i < 4; i++)
   {
     if(!digitalRead(motorButtons[i]))
     {
       if(i == 3)
       {
-        geneva_Speed[0] = 0;
+        GenevaMotor.drive(0);
       }
       else
       {
-        chem_Speeds[i] = 0;
+        Chem1Motor.drive(0);
+        Chem2Motor.drive(0);
+        Chem3Motor.drive(0);
       }
     }
   }
-  
+}
+
+void Estop()
+{
+  Serial.println("Estop triggered");
+  CheckButtonsEstop();
   Serial.println("Watchdog cleared");
+  WatchdogClear();
+}
+
+void WatchdogClear()
+{
   Watchdog.clear();
 }
