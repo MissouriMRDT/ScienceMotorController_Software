a36a46ec0795c71ca85a6c3d92c6edba8149ab90
Robert Squires <robert.squires01@gmail.com>
2022-04-24 17:31:33 -0500

    Better formatting, comments

--- ScienceMotorController/ScienceMotorController.h
+++ ScienceMotorController/ScienceMotorController.h
@@ -5,90 +5,98 @@
 #include "RoveStmVnhPwm.h"
 #include "RoveUsDigiMa3Pwm.h"
 #include "RoveWatchdog.h"
-#include "Servo.h"
 
 // motor pin declarations
 
-#define MOTOR_1_PWM PD_1
-#define MOTOR_1_IN_A PL_0
-#define MOTOR_1_IN_B PL_1
-#define MOTOR_1_CS PE_0
+#define MOTOR_1_PWM             PD_1
+#define MOTOR_1_IN_A            PL_0
+#define MOTOR_1_IN_B            PL_1
+#define MOTOR_1_CS              PE_0
 
-#define MOTOR_2_PWM PM_4
-#define MOTOR_2_IN_A PK_4
-#define MOTOR_2_IN_B PG_1
-#define MOTOR_2_CS PE_1
+#define MOTOR_2_PWM             PM_4
+#define MOTOR_2_IN_A            PK_4
+#define MOTOR_2_IN_B            PG_1
+#define MOTOR_2_CS              PE_1
 
-#define MOTOR_3_PWM PM_5
-#define MOTOR_3_IN_A PF_2
-#define MOTOR_3_IN_B PF_1
-#define MOTOR_3_CS PE_2
+#define MOTOR_3_PWM             PM_5
+#define MOTOR_3_IN_A            PF_2
+#define MOTOR_3_IN_B            PF_1
+#define MOTOR_3_CS              PE_2
 
-#define MOTOR_4_PWM PL_4
-#define MOTOR_4_IN_A PH_0
-#define MOTOR_4_IN_B PH_1
-#define MOTOR_4_CS PE_3
+#define MOTOR_4_PWM             PL_4
+#define MOTOR_4_IN_A            PH_0
+#define MOTOR_4_IN_B            PH_1
+#define MOTOR_4_CS              PE_3
 
 // servo pin declaration
 
-#define SERVO_1 PB_2
-#define SERVO_2 PB_3
+#define SERVO_1                 PB_2
+#define SERVO_2                 PB_3
 
 // limit switch pin declarations
 
-#define LIM_SWITCH_1 PE_5
-#define LIM_SWITCH_2 PC_4
-#define LIM_SWITCH_3 PC_5
-#define LIM_SWITCH_4 PC_6
-#define LIM_SWITCH_5 PD_3
-#define LIM_SWITCH_6 PC_7
+#define LIM_SWITCH_1            PE_5
+#define LIM_SWITCH_2            PC_4
+#define LIM_SWITCH_3            PC_5
+#define LIM_SWITCH_4            PC_6
+#define LIM_SWITCH_5            PD_3
+#define LIM_SWITCH_6            PC_7
 
 // encoder pin declarations
 
-#define ENC_1 PA_6
-#define ENC_2 PD_2
-#define ENC_3 PD_5
-#define ENC_4 PD_3
-#define ENC_5 PA_4
-#define ENC_6 PA_5
+#define ENC_1                   PA_6
+#define ENC_2                   PD_2
+#define ENC_3                   PD_5
+#define ENC_4                   PD_3
+#define ENC_5                   PA_4
+#define ENC_6                   PA_5
 
 // solenoid pins
 
-#define SOL_1 PB_4
-#define SOL_2 PP_0
-#define SOL_3 PB_5
-#define SOL_4 PP_1
-#define SOL_5 PK_0
-#define SOL_6 PD_4
-#define SOL_7 PK_1
-#define SOL_8 PK_2
-#define SOL_9 PK_3
+#define SOL_1                   PB_4
+#define SOL_2                   PP_0
+#define SOL_3                   PB_5
+#define SOL_4                   PP_1
+#define SOL_5                   PK_0
+#define SOL_6                   PD_4
+#define SOL_7                   PK_1
+#define SOL_8                   PK_2
+#define SOL_9                   PK_3
 
 // error LED pins
 
-#define ERR_1 PQ_0
-#define ERR_2 PP_4
+#define ERR_1                   PQ_0
+#define ERR_2                   PP_4
 
 // parameters
 
-#define SCOOP_OPEN_VALUE 115
-#define SCOOP_CLOSED_VALUE 150
+#define SCOOP_OPEN_VALUE        115 //PWM value (0-255) corresponding to scoop open position
+#define SCOOP_CLOSED_VALUE      150 //PWM value (0-255) corresponding to scoop closed position
+
+// RoveComm object declarations
 
 RoveCommEthernet RoveComm;
 EthernetServer TCPServer(RC_ROVECOMM_SCIENCEACTUATIONBOARD_PORT);
-RoveWatchdog Watchdog;
 rovecomm_packet packet;
+
+// motor controller object declarations
 
 RoveStmVnhPwm sensZ;
 RoveStmVnhPwm gantX;
 RoveStmVnhPwm gantZ;
+RoveStmVnhPwm spare;
+
 int16_t gantXTarget;
 int16_t gantZTarget;
 int16_t sensZTarget;
-RoveStmVnhPwm spare;
+
 RoveUsDigiMa3Pwm encoder[6];
+
 bool solStates[3];
+
 uint8_t sol[3][3] = {{SOL_1, SOL_2, SOL_3}, {SOL_4, SOL_5, SOL_6}, {SOL_7, SOL_8, SOL_9}};
+
 int16_t scoopAngle = 0;
 int16_t lastScoopAngle = 1;
+
 #endif
--- ScienceMotorController/ScienceMotorController.ino
+++ ScienceMotorController/ScienceMotorController.ino
@@ -2,14 +2,21 @@
 
 void setup()
 {
-  Serial.begin(9600);
   RoveComm.begin(RC_SCIENCEACTUATIONBOARD_FOURTHOCTET, &TCPServer);
+
+  // attack motor controllers to pins
   sensZ.attach(MOTOR_1_IN_A, MOTOR_1_IN_B, MOTOR_1_PWM);
   gantX.attach(MOTOR_2_IN_A, MOTOR_2_IN_B, MOTOR_2_PWM);
   gantZ.attach(MOTOR_3_IN_A, MOTOR_3_IN_B, MOTOR_3_PWM);
+
+  // set output pin modes for servos and solenoids
+  pinMode(SERVO_1, OUTPUT);
   pinMode(SERVO_2, OUTPUT);
-  for(int i = 0; i < 3; i ++) {
-    for(int a = 0; a < 3; a ++) {
+
+  for(int i = 0; i < 3; i ++)
+  {
+    for(int a = 0; a < 3; a ++)
+    {
       pinMode(sol[a][i], OUTPUT);
     }
   }
@@ -21,48 +28,71 @@ void loop()
 
   switch(packet.data_id)
   {
-    case RC_SCIENCEACTUATIONBOARD_SENSORAXIS_DATA_ID: {
+    case RC_SCIENCEACTUATIONBOARD_SENSORAXIS_DATA_ID:
+    {
+      // set sensor axis target value
       sensZTarget = ((int16_t*)packet.data)[0];
       break;
     }
     
-    case RC_SCIENCEACTUATIONBOARD_WATER_DATA_ID: {
+    case RC_SCIENCEACTUATIONBOARD_WATER_DATA_ID:
+    {
       int8_t solTarget = ((int8_t*)packet.data)[0];
-      for(int i = 0; i < 3; i ++) {
+      // TODO: add control for individual solenoids once RoveComm Manifest is updated for it
+
+      // increment through 3 solenoid groups
+      for(int i = 0; i < 3; i ++)
+      {
+        // check corresponding bit then shift right 1 bit
         solStates[i] = solTarget % 2;
         solTarget >> 1;
-        for(int a = 0; a < 3; a ++) {
+
+        // increment though each solenoid in group and set to correct state
+        for(int a = 0; a < 3; a ++)
+        {
           digitalWrite(sol[i][a], solStates[i]);
         }
       }
       break;
     }
 
-    case RC_SCIENCEACTUATIONBOARD_XOOPAXIS_DATA_ID: {
+    case RC_SCIENCEACTUATIONBOARD_XOOPAXIS_DATA_ID:
+    {
+      // TODO: add PID control based off encoder position once installed
+      // set gantry X axis target value
       gantXTarget = ((int16_t*)packet.data)[0];
       break;
     }
 
-    case RC_SCIENCEACTUATIONBOARD_ZOOPAXIS_DATA_ID: {
+    case RC_SCIENCEACTUATIONBOARD_ZOOPAXIS_DATA_ID:
+    {
+      // set gantry Z axis target value
       gantZTarget = ((int16_t*)packet.data)[0];
       break;
     }
 
-    case RC_SCIENCEACTUATIONBOARD_SCOOPGRABBER_DATA_ID: {
+    case RC_SCIENCEACTUATIONBOARD_SCOOPGRABBER_DATA_ID:
+    {
       scoopAngle = ((int16_t*)packet.data)[0];
-      Serial.println(scoopAngle);
-      if(scoopAngle != lastScoopAngle) {
-        analogWrite(SERVO_2, scoopAngle == 0 ? 115 : 150);
+
+      // check if scoop needs to change angle (repeatedly setting servo to the same value caused shakiness in testing)
+      if(scoopAngle != lastScoopAngle)
+      {
+        // set PWM pin to appropriate value
+        analogWrite(SERVO_2, scoopAngle == 0 ? SCOOP_OPEN_VALUE : SCOOP_CLOSED_VALUE);
         lastScoopAngle = scoopAngle;
       }
       break;
     }
 
-    default: {
+    default:
+    {
       break;
     }
   }
 
+  // TODO: add something to stop axes drifting due to gravity when target set to 0 using encoders (PID?)
+  // set each moco to target
   sensZ.drive(sensZTarget);
   gantX.drive(gantXTarget);
   gantZ.drive(gantZTarget);
